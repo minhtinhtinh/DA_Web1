599908dce88bf23a31701976a5d9d5d708f44935
minhtinh <nhutminh12a21998@gmail.com>
2019-01-14 00:42:39 +0700

    delete

--- Kidplazada_website/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $_SESSION["plazadakid_dangnhap"]="0";
-    $_SESSION["plazadakid_user"]="0";
-    session_destroy();
-    $url=$_GET["selfpage"];
-    header("Location: $url");
-?>
